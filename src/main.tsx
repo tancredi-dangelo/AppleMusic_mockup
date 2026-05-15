8338e8b90832ee81359a8a8f5d6d3310dc907002
Tancredi <tancredi.dangelo.22@gmail.com>
2026-05-15 09:41:28 +0200

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App.tsx";
+/*import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";*/
+
+createRoot(document.getElementById("root")!).render(
+  /*<Provider store={store}>
+    <PersistGate persistor={persistedStore}>*/
+  <App />,
+  /*</PersistGate>
+  </Provider>,*/
+);
